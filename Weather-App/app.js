156fe81c231f74c7956dc862e4d61fdbee54d87d
Surendiran S <44779174+surenyas1902@users.noreply.github.com>
2020-05-21 10:11:33 +0530

    Lat and Long is fetched in the service and Error Handling

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -3,6 +3,30 @@ const request = require("request")
 
 const url = "http://api.weatherstack.com/current?access_key=c5f720cf5f8eb1da3a43be7a606b7e03&query=Arakkonam&units=f"
 request({url: url, json: true}, (error, response) => {
+    if (error) {
+        console.log("Unable to connect to weather services.");
+        return;
+    } else if (response.body.success === false) {
+        console.log("Unable to find location")
+        return;
+    }
     const current = response.body.current;
     console.log(current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. It feels like "+ current.feelslike + " degrees out.")
+})
+
+const coordurl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Arakkonam.json?access_token=pk.eyJ1Ijoic3VyZW55YXMxOTAyIiwiYSI6ImNrYWJ5ZHRreDFiY3Iyc210dnRjbHB3Y2wifQ.kitN6lIZN2RDQv78GFQyaw&limit=1";
+request({url: coordurl, json: true}, (error,response) => {
+    if (error) {
+        console.log("Unable to connect to weather services.");
+        return;
+    }
+    else if (response.body.features.length === 0) {
+        console.log("Unable to find location");
+        return;
+    }
+    const data = response.body;
+    const getCoordinates = data.features[0].center;
+    const lat = getCoordinates[1]
+    const long = getCoordinates[0]
+    console.log("Latitude: " + lat + ", Longitude: " + long)
 })
