55fb58442c5c4dc1ef3bbf4ce46677fc01d65534
Surendiran S <44779174+surenyas1902@users.noreply.github.com>
2020-05-28 10:24:34 +0530

    Handlebars added for Web Server Application

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,39 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 const publicDir = path.join(__dirname,'../public')
-app.use(express.static(publicDir))
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine','hbs') //Setting a view engine to run
+app.set('views', viewsPath) //Settings Views Path/Location.
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDir)) //For Static Html Paths for Static Websites.
 
 app.get('', (req, res) => { // request and response
-    res.send('<h1>Weather</h1>')
+    res.render('index', {
+        title:"Weather",
+        name: "SurendiranS"
+    }) // Name of the view to render
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About Me",
+        name:"Surendiran.S"
+    })
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: "Please read the FAQ for the questions.",
+        title: "Help",
+        name: "Surendiran S"
+    })
 })
 
 app.get('/weather', (req, res) => {
